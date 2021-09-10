09f2fc0526eed3abf44156f6f56912ee4f5c0a55
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-09-10 10:32:51 +0200

    ex 1.4

--- js/test.js
+++ js/test.js
@@ -1,15 +1,6 @@
 const readlineSync = require("readline-sync");
 
 
-// EX - 1.4  ----------------------------------------------------------------------------------------------------
-
-/* let name = readlineSync.question('Can you give me your name please?');
-let firstName = readlineSync.question('Can you give me your firstname please?');
-let city = readlineSync.question('Where do you live ?');
-
-console.log("Your name is", firstName, name, "and you live in", city,"."); */
-
-
 // EX - 1.5  ----------------------------------------------------------------------------------------------------
 
 
