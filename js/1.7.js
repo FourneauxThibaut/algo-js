356970605c89f0c20100b46bd3d162e136e09c07
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-09-10 10:38:22 +0200

    ex 1.7

--- js/1.7.js
+++ js/1.7.js
@@ -1,17 +1,27 @@
 const readlineSync = require("readline-sync");
 
-// EX - 1.7  ----------------------------------------------------------------------------------------------------
+// EX - 1.8  ----------------------------------------------------------------------------------------------------
 
 
-let shoeSize = readlineSync.question('Can you the give me your shoe size please ?');
-let birthYear = readlineSync.question('Can you the give me your birth year please ?');
+let questions = 4;
 
-let shoeSizeDouble = shoeSize * 2;
+console.log( questions + ' remaining');
+let name = readlineSync.question('Can you give me your name please?');
+questions -= 1;
 
-shoeSizeDouble += 5;
-shoeSizeDouble *= 50;
+console.log( questions, ' remaining');
+let firstName = readlineSync.question('Can you give me your firstname please?');
 
-shoeSizeDouble -= birthYear;
-shoeSizeDouble += 1766;
+questions -= 1;
+console.log( questions, ' remaining');
+let city = readlineSync.question('Where do you live ?');
 
-console.log(shoeSizeDouble);
+questions -= 1;
+console.log( questions, ' remaining');
+let study = readlineSync.question('What did you study ?');
+
+
+console.log(' ');
+console.log(' your name is ', name + firstName);
+console.log(' and you did learn', study, 'and kunfu.. no ?');
+console.log(' you\'r now living in ', city);
