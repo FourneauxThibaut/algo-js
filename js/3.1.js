8daa6cce53ffceeab80d6aef87c413e3d8901823
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-09-10 14:48:44 +0200

    ex 3.1

---
+++ js/3.1.js
@@ -0,0 +1,18 @@
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [100, 101, 102];
+
+
+let total = 0;
+
+arr1.forEach(function(item){
+    total += parseInt(item);
+});
+
+console.log('the total of your number are', total);
+total = 0;
+
+arr2.forEach(function(item){
+    total += parseInt(item);
+});
+
+console.log('the total of your number are', total);
