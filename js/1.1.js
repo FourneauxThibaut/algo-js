3aedb897b611b058facecc338fddc585b258b6df
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-09-10 10:26:24 +0200

    ex 1.1

---
+++ js/1.1.js
@@ -0,0 +1,8 @@
+const readlineSync = require("readline-sync");
+
+// EX - 1.1  ----------------------------------------------------------------------------------------------------
+
+
+let variable = "oui";
+console.log(variable);
+
