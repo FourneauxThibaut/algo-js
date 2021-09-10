f53df6ca29827679b25477cca65072057276fc51
fourneauxthibaut <fourneaux.thibaut@gmail.com>
2021-09-10 10:31:23 +0200

    ex 1.2

---
+++ js/1.2.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync");
+
+// EX - 1.2  ----------------------------------------------------------------------------------------------------
+
+
+let name = "Lambert";
+let firstName = "Gerard";
+let city = "Paris";
+
+
+console.log("Your name is", firstName, name, "and you live in", city,".");
